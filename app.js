f7705817d4c09db29c5c68b95f077c5f0aa01e34
Alexey <advreid@yandex.ru>
2023-01-30 18:41:08 +1000

    default commit

--- app.js
+++ app.js
@@ -1,42 +1,8 @@
-let userOnline = true, intervalId, timer = 10;
+
 const popup = document.querySelector('.popup');
 const wifiImg = document.querySelector('.uil');
-
-
-// Проверим есть ли соединение 
-async function checkOnline() {
-    try {
-        //Попробуем получить запрос
-        const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-        userOnline = response.status >= 200 && response.status < 300;
-    }
-    catch (error) {
-        userOnline = false;
-    }
-    statusz(userOnline);
-}
-
-
-function statusz(status) {
-    if (!status) {
-        getIp();
-        document.querySelector('.title').textContent = `Вы в сети, Отлично!`;
-        document.querySelector('.title').classList.remove('red');
-    }
-
-    else if (status) {
-        document.querySelector('.title').textContent = `Вы не в сети..`;
-        document.querySelector('.title').classList.add('red');
-        document.querySelector('.ip').textContent = "Неизвестно"
-        document.querySelector('.country').textContent = "Неизвестно"
-        document.querySelector('.city').textContent = "Неизвестно"
-    }
-
-}
-
-
-
-checkOnline();
+const update = document.querySelector('.reconnect');
+getIp();
 
 // Получим IP пользователя;
 function getIp() {
@@ -45,25 +11,15 @@ function getIp() {
     <p class="desc">Страна: <span class="country">${result.country}</span></p>
     <p class="desc">Город: <span class="city">${result.city}</span></p>
     </p>`;
-
         console.log(result);
     }
     )
 }
-getIp();
 
-
-
-
-
-document.querySelector('.reconnect').addEventListener('click', function () {
-
+update.addEventListener('click', function () {
     document.querySelector('.loading-con').innerHTML = `<img class="loading" src = "loading.gif">`;
-    statusz();
-
-}
-
-)
+    getIp();
+})
 
 
 
